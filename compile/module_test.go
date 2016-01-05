a822449cab8f19387ff43cd7a9b56a0376350f66
Abhinav Gupta <abg@uber.com>
2016-01-04 16:55:49 -0800

    Include module name in Lookup* errors

--- compile/module_test.go
+++ compile/module_test.go
@@ -82,31 +82,60 @@ func TestLookupTypeFailure(t *testing.T) {
 	}{
 		{
 			"unknown identifier",
-			&Module{},
+			&Module{Name: "some_module"},
 			"foo",
-			[]string{`unknown identifier "foo"`},
+			[]string{`unknown identifier "foo" in "some_module"`},
 		},
 		{
 			"unknown module",
-			&Module{},
+			&Module{Name: "some_module"},
 			"shared.UUID",
 			[]string{
-				`unknown identifier "shared.UUID"`,
+				`unknown identifier "shared.UUID" in "some_module"`,
 				`unknown module "shared"`,
 			},
 		},
 		{
 			"unknown identifier in included module",
-			&Module{Includes: map[string]*IncludedModule{
-				"shared": {
-					Name:   "shared",
-					Module: &Module{},
+			&Module{
+				Name: "foo",
+				Includes: map[string]*IncludedModule{
+					"shared": {
+						Name:   "shared",
+						Module: &Module{Name: "shared"},
+					},
 				},
-			}},
+			},
 			"shared.UUID",
 			[]string{
-				`unknown identifier "shared.UUID"`,
-				`unknown identifier "UUID"`,
+				`unknown identifier "shared.UUID" in "foo"`,
+				`unknown identifier "UUID" in "shared"`,
+			},
+		},
+		{
+			"unknown identifier in included module of included module",
+			&Module{
+				Name: "foo",
+				Includes: map[string]*IncludedModule{
+					"bar": {
+						Name: "bar",
+						Module: &Module{
+							Name: "bar",
+							Includes: map[string]*IncludedModule{
+								"baz": {
+									Name:   "baz",
+									Module: &Module{Name: "baz"},
+								},
+							},
+						},
+					},
+				},
+			},
+			"bar.baz.Qux",
+			[]string{
+				`unknown identifier "bar.baz.Qux" in "foo"`,
+				`unknown identifier "baz.Qux" in "bar"`,
+				`unknown identifier "Qux" in "baz"`,
 			},
 		},
 	}
@@ -140,6 +169,7 @@ func TestLookupConstant(t *testing.T) {
 		{
 			"simple constant lookup",
 			&Module{
+				Name: "foo",
 				Constants: map[string]*Constant{
 					"Version": {
 						Name:  "Version",
@@ -154,8 +184,10 @@ func TestLookupConstant(t *testing.T) {
 		{
 			"included constant lookup",
 			&Module{
+				Name: "foo",
 				Includes: map[string]*IncludedModule{
 					"shared": {Name: "shared", Module: &Module{
+						Name: "shared",
 						Constants: map[string]*Constant{
 							"DefaultUser": {
 								Name:  "DefaultUser",
@@ -171,15 +203,17 @@ func TestLookupConstant(t *testing.T) {
 		},
 		{
 			"enum constant lookup",
-			&Module{Types: map[string]TypeSpec{"Role": role}},
+			&Module{Name: "foo", Types: map[string]TypeSpec{"Role": role}},
 			"Role.Moderator",
 			ast.ConstantInteger(2),
 		},
 		{
 			"included enum constant lookup",
 			&Module{
+				Name: "foo",
 				Includes: map[string]*IncludedModule{
 					"shared": {Name: "shared", Module: &Module{
+						Name:  "shared",
 						Types: map[string]TypeSpec{"Role": role},
 					}},
 				},
@@ -206,47 +240,53 @@ func TestLookupConstantFailure(t *testing.T) {
 	}{
 		{
 			"unknown identifier",
-			&Module{},
+			&Module{Name: "bar"},
 			"foo",
-			[]string{`unknown identifier "foo"`},
+			[]string{`unknown identifier "foo" in "bar"`},
 		},
 		{
 			"unknown module",
-			&Module{},
+			&Module{Name: "bar"},
 			"shared.DEFAULT_UUID",
 			[]string{
-				`unknown identifier "shared.DEFAULT_UUID"`,
+				`unknown identifier "shared.DEFAULT_UUID" in "bar"`,
 				`unknown module "shared"`,
 			},
 		},
 		{
 			"unknown identifier in included module",
-			&Module{Includes: map[string]*IncludedModule{
-				"shared": {
-					Name:   "shared",
-					Module: &Module{},
+			&Module{
+				Name: "foo",
+				Includes: map[string]*IncludedModule{
+					"shared": {
+						Name:   "shared",
+						Module: &Module{Name: "shared"},
+					},
 				},
-			}},
+			},
 			"shared.DEFAULT_UUID",
 			[]string{
-				`unknown identifier "shared.DEFAULT_UUID"`,
-				`unknown identifier "DEFAULT_UUID"`,
+				`unknown identifier "shared.DEFAULT_UUID" in "foo"`,
+				`unknown identifier "DEFAULT_UUID" in "shared"`,
 			},
 		},
 		{
 			"unknown item in enum",
-			&Module{Types: map[string]TypeSpec{
-				"Foo": &EnumSpec{
-					Name: "Foo",
-					Items: []EnumItem{
-						{Name: "A", Value: 1},
-						{Name: "B", Value: 2},
+			&Module{
+				Name: "foo",
+				Types: map[string]TypeSpec{
+					"Foo": &EnumSpec{
+						Name: "Foo",
+						Items: []EnumItem{
+							{Name: "A", Value: 1},
+							{Name: "B", Value: 2},
+						},
 					},
 				},
-			}},
+			},
 			"Foo.C",
 			[]string{
-				`unknown identifier "Foo.C"`,
+				`unknown identifier "Foo.C" in "foo"`,
 				`enum "Foo" does not have an item named "C"`,
 			},
 		},
@@ -289,6 +329,7 @@ func TestLookupService(t *testing.T) {
 		{
 			"simple service lookup",
 			&Module{
+				Name:     "foo",
 				Services: map[string]*ServiceSpec{"SomeService": someService},
 			},
 			"SomeService",
@@ -297,10 +338,12 @@ func TestLookupService(t *testing.T) {
 		{
 			"included service lookup",
 			&Module{
+				Name: "foo",
 				Includes: map[string]*IncludedModule{
 					"bar": {
 						Name: "bar",
 						Module: &Module{
+							Name: "bar",
 							Services: map[string]*ServiceSpec{
 								"SomeService": someService,
 							},
@@ -337,30 +380,31 @@ func TestLookupServiceFailure(t *testing.T) {
 	}{
 		{
 			"unknown service",
-			&Module{},
+			&Module{Name: "foo"},
 			"Foo",
-			[]string{`unknown identifier "Foo"`},
+			[]string{`unknown identifier "Foo" in "foo"`},
 		},
 		{
 			"unknown module",
-			&Module{},
+			&Module{Name: "bar"},
 			"foo.Bar",
 			[]string{
-				`unknown identifier "foo.Bar"`,
+				`unknown identifier "foo.Bar" in "bar"`,
 				`unknown module "foo"`,
 			},
 		},
 		{
 			"unknown service in included module",
 			&Module{
+				Name: "bar",
 				Includes: map[string]*IncludedModule{
-					"foo": {Name: "foo", Module: &Module{}},
+					"foo": {Name: "foo", Module: &Module{Name: "foo"}},
 				},
 			},
 			"foo.Bar",
 			[]string{
-				`unknown identifier "foo.Bar"`,
-				`unknown identifier "Bar"`,
+				`unknown identifier "foo.Bar" in "bar"`,
+				`unknown identifier "Bar" in "foo"`,
 			},
 		},
 	}
