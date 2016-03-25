af416fcb45d8568cfbcd720ec89d8271c3872ff6
Abhinav Gupta <abg@uber.com>
2016-03-25 14:05:42 -0700

    Move bulk of struct generation logic out
    This will allow this logic to be re-used for function args and result structs.

--- gen/struct.go
+++ gen/struct.go
@@ -20,7 +20,10 @@
 
 package gen
 
-import "github.com/thriftrw/thriftrw-go/compile"
+import (
+	"github.com/thriftrw/thriftrw-go/ast"
+	"github.com/thriftrw/thriftrw-go/compile"
+)
 
 // structGenerator generates code to serialize and deserialize structs.
 type structGenerator struct {
@@ -55,124 +58,44 @@ func (s *structGenerator) Reader(g Generator, spec *compile.StructSpec) (string,
 }
 
 func structure(g Generator, spec *compile.StructSpec) error {
-	err := g.DeclareFromTemplate(
-		`
-		<$fmt := import "fmt">
-		<$strings := import "strings">
-		<$wire := import "github.com/thriftrw/thriftrw-go/wire">
-		<$structName := typeName .>
-		<$structRef := typeReference .>
+	fg := fieldGroupGenerator{
+		Name:   goCase(spec.Name),
+		Fields: spec.Fields,
+	}
 
-		type <$structName> struct {
-		<range .Fields>
-			<if .Required>
-				<goCase .Name> <typeReference .Type>
-			<else>
-				<goCase .Name> <typeReferencePtr .Type>
-			<end>
-		<end>
-		}
+	if err := fg.DefineStruct(g); err != nil {
+		return wrapGenerateError(spec.ThriftName(), err)
+	}
 
+	if err := fg.ToWire(g); err != nil {
+		return wrapGenerateError(spec.ThriftName(), err)
+	}
 
-		<$v := newVar "v">
-		<$i := newVar "i">
-		<$fields := newVar "fs">
-		func (<$v> <$structRef>) ToWire() <$wire>.Value {
-			// TODO check if required fields that are reference types are nil
-			var <$fields> [<len .Fields>]<$wire>.Field
-			<$i> := 0
+	if err := fg.FromWire(g); err != nil {
+		return wrapGenerateError(spec.ThriftName(), err)
+	}
 
-			<range .Fields>
-				<$f := printf "%s.%s" $v (goCase .Name)>
+	if err := fg.String(g); err != nil {
+		return wrapGenerateError(spec.ThriftName(), err)
+	}
 
-				<if .Required>
-					<$wVal := toWire .Type $f>
-					<$fields>[<$i>] = <$wire>.Field{ID: <.ID>, Value: <$wVal>}
-					<$i>++
-				<else>
-					if <$f> != nil {
-						<$fields>[<$i>] = <$wire>.Field{
-							ID: <.ID>,
-							Value: <toWirePtr .Type $f>,
-						}
-						<$i>++
-					}
-				<end>
-			<end>
-
-			return <$wire>.NewValueStruct(
-				<$wire>.Struct{Fields: <$fields>[:<$i>]},
-			)
-		}
-
-		<$w := newVar "w">
-		func (<$v> <$structRef>) FromWire(<$w> <$wire>.Value) error {
-			var err error
-			<$f := newVar "f">
-			for _, <$f> := range <$w>.GetStruct().Fields {
-				switch <$f>.ID {
-				<range .Fields>
-				case <.ID>:
-					if <$f>.Value.Type() == <typeCode .Type> {
-						<$lhs := printf "%s.%s" $v (goCase .Name)>
-						<$value := printf "%s.Value" $f>
-						<if .Required>
-							<$lhs>, err = <fromWire .Type $value>
-						<else>
-							<fromWirePtr .Type $lhs $value>
-						<end>
-						if err != nil {
-							return err
-						}
-					}
-				<end>
-				}
-			}
-
-			// TODO(abg): Check that all required fields were set.
-			return nil
-		}
-
-		func (<$v> <$structRef>) String() string {
-			var <$fields> [<len .Fields>]string
-			<$i> := 0
-			<range .Fields>
-				<$f := printf "%s.%s" $v (goCase .Name)>
-
-				<if not .Required>
-					if <$f> != nil {
-						<if isPrimitiveType .Type>
-							<$fields>[<$i>] = <$fmt>.Sprintf("<goCase .Name>: %v", *(<$f>))
-						<else>
-							<$fields>[<$i>] = <$fmt>.Sprintf("<goCase .Name>: %v", <$f>)
-						<end>
-						<$i>++
-					}
-				<else>
-					<$fields>[<$i>] = <$fmt>.Sprintf("<goCase .Name>: %v", <$f>)
-					<$i>++
-				<end>
-			<end>
-
-			return <$fmt>.Sprintf(
-				"<$structName>{%v}",
-				<$strings>.Join(<$fields>[:<$i>], ", "),
-			)
-		}
-
-		<if .IsExceptionType>
-			func (<$v> <$structRef>) Error() string {
+	if spec.Type == ast.ExceptionType {
+		err := g.DeclareFromTemplate(
+			`
+			<$v := newVar "v">
+			func (<$v> *<typeName .>) Error() string {
 				return <$v>.String()
 			}
-		<end>
-		`,
-		spec,
-	)
+			`, spec)
+		if err != nil {
+			return wrapGenerateError(spec.ThriftName(), err)
+		}
+	}
+
+	return nil
 	// TODO(abg): JSON tags for generated structs
 	// TODO(abg): For all struct types, handle the case where fields are named
 	// ToWire or FromWire.
 	// TODO(abg): For exceptions, handle the case where a field is named
 	// Error.
-
-	return wrapGenerateError(spec.Name, err)
 }
