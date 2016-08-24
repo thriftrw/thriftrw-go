5c88d0a8eab5cd768740047d6c19dc860cf38b84
Abhinav Gupta <mail@abhinavg.net>
2016-08-23 17:56:14 -0700

    internal: Add multiplex.{Client,Handler} (#181)
    This provides, `multiplex.{Client, Handler}` which are
    `TMultiplexedProtocol`-compatible versions of the `envelope.{Client, Handler}`
    interfaces from #180.

---
+++ internal/multiplex/client_test.go
@@ -0,0 +1,24 @@
+package multiplex
+
+import (
+	"testing"
+
+	"github.com/thriftrw/thriftrw-go/internal/envelope/envelopetest"
+	"github.com/thriftrw/thriftrw-go/wire"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockClient := envelopetest.NewMockClient(mockCtrl)
+	mockClient.EXPECT().Send("Foo:hello", wire.NewValueStruct(wire.Struct{})).
+		Return(wire.NewValueStruct(wire.Struct{}), nil)
+
+	client := NewClient("Foo", mockClient)
+	_, err := client.Send("hello", wire.NewValueStruct(wire.Struct{}))
+	assert.NoError(t, err)
+}
---
+++ internal/multiplex/handler_test.go
@@ -0,0 +1,64 @@
+package multiplex
+
+import (
+	"testing"
+
+	"github.com/thriftrw/thriftrw-go/internal/envelope"
+	"github.com/thriftrw/thriftrw-go/internal/envelope/envelopetest"
+	"github.com/thriftrw/thriftrw-go/wire"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandlerErrors(t *testing.T) {
+	tests := []struct {
+		name      string
+		expect    func(meta, kv *envelopetest.MockHandler)
+		wantError error
+	}{
+		{
+			name: "Meta:health",
+			expect: func(meta, kv *envelopetest.MockHandler) {
+				meta.EXPECT().Handle("health", gomock.Any()).
+					Return(wire.NewValueStruct(wire.Struct{}), nil)
+			},
+		},
+		{
+			name: "KeyValue:setValue",
+			expect: func(meta, kv *envelopetest.MockHandler) {
+				kv.EXPECT().Handle("setValue", gomock.Any()).
+					Return(wire.NewValueStruct(wire.Struct{}), nil)
+			},
+		},
+		{
+			name:      "keyValue:setValue",
+			wantError: envelope.ErrUnknownMethod("keyValue:setValue"),
+		},
+		{
+			name:      "setValue",
+			wantError: envelope.ErrUnknownMethod("setValue"),
+		},
+	}
+
+	for _, tt := range tests {
+		func() {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			meta := envelopetest.NewMockHandler(mockCtrl)
+			kv := envelopetest.NewMockHandler(mockCtrl)
+
+			handler := NewHandler()
+			handler.Put("Meta", meta)
+			handler.Put("KeyValue", kv)
+
+			if tt.expect != nil {
+				tt.expect(meta, kv)
+			}
+
+			_, err := handler.Handle(tt.name, wire.NewValueStruct(wire.Struct{}))
+			assert.Equal(t, tt.wantError, err)
+		}()
+	}
+}
