2c0286ac525a5c3295db48a0e491423c3484ada3
Abhinav Gupta <abg@uber.com>
2016-08-29 15:10:43 -0700

    plugins: Parse command line arguments
    This adds support for parsing plugin arguments over the command line. It does
    not actually integrate into the code generator yet.
    
    Plugins are specified over the command line using the "-p" or "--plugin"
    options. The value is in the form,
    
        NAME [ARGS ...]
    
    Where `NAME` is the name of the plugin and `ARGS` is zero or more arguments to
    pass to the plugin.
    
    An executable with the name `thriftrw-plugin-${NAME}` is expected to be
    available on `$PATH` to call the plugin. `ARGS` are passed to the plugin
    executable.
    
    For example,
    
        -p yarpc -p 'accessors --no-setters'
    
    Will call,
    
    -   `thrifrw-plugin-yarpc`
    -   `thriftrw-plugin-accessors --no-setters`
    
    Currently, a plugin can only be called once in a single invocation of
    ThriftRW. That is, the following is currently unsupported:
    
        -p "foo --someflag" -p "foo --anotherflag"

---
+++ internal/plugin/flag_test.go
@@ -0,0 +1,286 @@
+package plugin
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testdata(t *testing.T, paths ...string) string {
+	cwd, err := os.Getwd()
+	require.NoError(t, err, "could not determine CWD")
+
+	args := []string{cwd, "testdata"}
+	args = append(args, paths...)
+	return filepath.Join(args...)
+}
+
+func prependToPath(p string) func() {
+	oldPath := os.Getenv("PATH")
+	newPath := p + string(os.PathListSeparator) + oldPath
+
+	os.Setenv("PATH", newPath)
+	return func() { os.Setenv("PATH", oldPath) }
+}
+
+func TestUnmarshalFlag(t *testing.T) {
+	done := prependToPath(testdata(t))
+	defer done()
+
+	tests := []struct {
+		giveValue string
+
+		wantName string
+		wantPath string
+		wantArgs []string
+
+		wantErrorPrefix string
+	}{
+		{
+			giveValue:       "",
+			wantErrorPrefix: `invalid plugin "": please provide a name`,
+		},
+		{
+			giveValue:       `foo '`,
+			wantErrorPrefix: `invalid plugin "foo '": No closing quotation`,
+		},
+		{
+			giveValue:       "unknown-plugin",
+			wantErrorPrefix: `invalid plugin "unknown-plugin": could not find executable "thriftrw-plugin-unknown-plugin":`,
+		},
+		{
+			giveValue: "empty",
+			wantName:  "empty",
+			wantPath:  testdata(t, "thriftrw-plugin-empty"),
+		},
+		{
+			giveValue: "handshake-failed",
+			wantName:  "handshake-failed",
+			wantPath:  testdata(t, "thriftrw-plugin-handshake-failed"),
+		},
+		{
+			giveValue: "handshake-failed --bar baz",
+			wantName:  "handshake-failed",
+			wantPath:  testdata(t, "thriftrw-plugin-handshake-failed"),
+			wantArgs:  []string{"--bar", "baz"},
+		},
+		{
+			giveValue: `handshake-failed bar\ baz`,
+			wantName:  "handshake-failed",
+			wantPath:  testdata(t, "thriftrw-plugin-handshake-failed"),
+			wantArgs:  []string{"bar baz"},
+		},
+		{
+			giveValue: `handshake-failed bar="baz qux"`,
+			wantName:  "handshake-failed",
+			wantPath:  testdata(t, "thriftrw-plugin-handshake-failed"),
+			wantArgs:  []string{"bar=baz qux"},
+		},
+	}
+
+	for _, tt := range tests {
+		var f Flag
+		err := f.UnmarshalFlag(tt.giveValue)
+		if tt.wantErrorPrefix != "" {
+			if assert.Error(t, err, "expected error for %q", tt.giveValue) {
+				assert.True(t,
+					strings.HasPrefix(err.Error(), tt.wantErrorPrefix),
+					"expected prefix %q on error message %q for %q",
+					tt.wantErrorPrefix, err.Error(), tt.giveValue,
+				)
+			}
+		} else {
+			if assert.NoError(t, err, "expected no error for %q", tt.giveValue) {
+				assert.Equal(t, tt.wantName, f.Name, "name for %q does not match", tt.giveValue)
+				assert.Equal(t, tt.wantPath, f.Command.Path, "path for %q does not match", tt.giveValue)
+
+				// We check args[1:] because args[0] is always path.
+				if len(tt.wantArgs) == 0 {
+					assert.Empty(t, f.Command.Args[1:], "args for %q do not match", tt.giveValue)
+				} else {
+					assert.Equal(t, tt.wantArgs, f.Command.Args[1:], "args for %q do not match", tt.giveValue)
+				}
+			}
+		}
+	}
+}
+
+func TestFlagHandle(t *testing.T) {
+	tests := []struct {
+		desc string
+		name string
+		path string
+		args []string
+
+		wantErrors []string
+	}{
+		{
+			desc: "working plugin",
+			name: "empty",
+			path: testdata(t, "thriftrw-plugin-empty"),
+		},
+		{
+			desc: "handshake failed",
+			name: "handshake-failed",
+			path: testdata(t, "thriftrw-plugin-handshake-failed"),
+			wantErrors: []string{
+				`failed to open plugin "handshake-failed":`,
+				`handshake with plugin "handshake-failed" failed:`,
+			},
+		},
+		{
+			desc:       "non existent path",
+			name:       "bar",
+			path:       testdata(t, "thriftrw-plugin-bar"),
+			wantErrors: []string{`failed to open plugin "bar":`, "no such file or directory"},
+		},
+	}
+
+	for _, tt := range tests {
+		f := Flag{
+			Name:    tt.name,
+			Command: exec.Command(tt.path, tt.args...),
+		}
+		h, err := f.Handle()
+
+		if len(tt.wantErrors) > 0 {
+			if !assert.Error(t, err, "%v: expected error", tt.desc) {
+				continue
+			}
+
+			for _, msg := range tt.wantErrors {
+				assert.Contains(t, err.Error(), msg, "%v: error message mismatch", tt.desc)
+			}
+		} else {
+			if assert.NoError(t, err, "%v: expected no error", tt.desc) {
+				assert.NoError(t, h.Close(), "%v: failed to close", tt.desc)
+			}
+		}
+	}
+}
+
+func TestFlagsHandle(t *testing.T) {
+	type plug struct {
+		name string
+		path string
+		args []string
+	}
+
+	tests := []struct {
+		desc  string
+		plugs []plug
+
+		wantNil    bool // both, handle and err must be nil
+		wantErrors []string
+	}{
+		{
+			desc:    "no plugins",
+			wantNil: true,
+		},
+		{
+			desc:    "empty plugins",
+			plugs:   []plug{},
+			wantNil: true,
+		},
+		{
+			desc: "duplicate plugin",
+			plugs: []plug{
+				{
+					name: "empty",
+					path: testdata(t, "thriftrw-plugin-empty"),
+				},
+				{
+					name: "empty",
+					path: testdata(t, "thriftrw-plugin-empty"),
+					args: []string{"extra", "args"},
+				},
+			},
+			wantErrors: []string{
+				`plugin conflict: plugin "empty" is specified multiple times`,
+			},
+		},
+		{
+			desc: "all success",
+			plugs: []plug{
+				{
+					name: "empty",
+					path: testdata(t, "thriftrw-plugin-empty"),
+				},
+				{
+					name: "another-empty",
+					path: testdata(t, "thriftrw-plugin-another-empty"),
+				},
+			},
+		},
+		{
+			desc: "all fail",
+			plugs: []plug{
+				{
+					name: "handshake-failed",
+					path: testdata(t, "thriftrw-plugin-handshake-failed"),
+				},
+				{
+					name: "another-handshake-failed",
+					path: testdata(t, "thriftrw-plugin-another-handshake-failed"),
+				},
+			},
+			wantErrors: []string{
+				`failed to open plugin "handshake-failed": handshake with plugin "handshake-failed" failed:`,
+				`failed to open plugin "another-handshake-failed": handshake with plugin "another-handshake-failed" failed:`,
+			},
+		},
+		{
+			desc: "partial failure",
+			plugs: []plug{
+				{
+					name: "handshake-failed",
+					path: testdata(t, "thriftrw-plugin-handshake-failed"),
+				},
+				{
+					name: "empty",
+					path: testdata(t, "thriftrw-plugin-empty"),
+				},
+			},
+			wantErrors: []string{
+				`failed to open plugin "handshake-failed": handshake with plugin "handshake-failed" failed:`,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		var flags Flags
+		for _, p := range tt.plugs {
+			flags = append(flags, Flag{
+				Name:    p.name,
+				Command: exec.Command(p.path, p.args...),
+			})
+		}
+
+		h, err := flags.Handle()
+
+		if tt.wantNil {
+			assert.Nil(t, h, "%v: expected nil handle", tt.desc)
+			assert.Nil(t, err, "%v: expected nil error", tt.desc)
+			continue
+		}
+
+		if len(tt.wantErrors) > 0 {
+			if !assert.Error(t, err, "%v: expected error", tt.desc) {
+				continue
+			}
+
+			for _, msg := range tt.wantErrors {
+				assert.Contains(t, err.Error(), msg, "%v: error message mismatch", tt.desc)
+			}
+		} else {
+			if assert.NoError(t, err, "%v: expected no error", tt.desc) {
+				assert.NoError(t, h.Close(), "%v: failed to close", tt.desc)
+			}
+		}
+	}
+}
