df897d944c2640669db335c8680e286c2b662415
Abhinav Gupta <abg@uber.com>
2016-11-17 14:16:04 -0800

    doc: Clarify plugin stdout/stdin contract
    This caused confusion for a plugin author.

--- plugin/doc.go
+++ plugin/doc.go
@@ -36,13 +36,21 @@
 // 	}
 //
 // Note that the name in the executable MUST match the name in the Plugin
-// struct. Plugins must be installed and available on the $PATH before they
-// can be used. To use a plugin, pass its name in with the -p/--plugin option.
+// struct. Plugins need to be installed and available on the $PATH before they
+// can be used. Additionaly, plugins talk to the ThriftRW process using stdout
+// and stdin. This means that plugin implementations MUST NOT write to stdout
+// or read from stdin. Plugins MAY write to stderr and receive input via
+// command line arguments.
+//
+// To use a plugin, pass its name in with the -p/--plugin option.
 //
 // 	thriftrw --plugin=myfancyplugin foo.thrift
 //
 // Arguments may be sent to plugins by including them in the name. These are
-// passed through to the plugin process as command line arguments.
+// passed through to the plugin process as command line arguments. For
+// example,
 //
 // 	thriftrw --plugin='myfancyplugin --useContext'
+//
+// Will pass `--useContext` to `thriftrw-plugin-myfancyplugin`.
 package plugin
