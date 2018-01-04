16f15c5de3f9a6e6ad4ce8721c613302dcbe3a78
Kris Kowal <kris@uber.com>
2018-01-03 17:53:02 -0800

    Remove versioncheck

--- gen/generate.go
+++ gen/generate.go
@@ -223,19 +223,6 @@ func generateModule(m *compile.Module, i thriftPackageImporter, builder *generat
 
 	g := NewGenerator(i, importPath, packageName)
 
-	if !o.NoVersionCheck {
-		if err := Version(g, importPath); err != nil {
-			return nil, err
-		}
-
-		var buff bytes.Buffer
-		if err := g.Write(&buff, nil /* fset */); err != nil {
-			return nil, fmt.Errorf(
-				"could not generate version check for %q: %v", m.ThriftPath, err)
-		}
-		files["versioncheck.go"] = buff.Bytes()
-	}
-
 	if len(m.Constants) > 0 {
 		for _, constantName := range sortStringKeys(m.Constants) {
 			if err := Constant(g, m.Constants[constantName]); err != nil {
