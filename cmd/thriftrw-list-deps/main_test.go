1cfb520c111040b94b4714e0b51bef85a81d4e8a
Jacob Greenleaf <jacobg@uber.com>
2018-08-15 13:15:54 -0700

    Clean up tests and make disk hard requirement

--- cmd/thriftrw-list-deps/main_test.go
+++ cmd/thriftrw-list-deps/main_test.go
@@ -16,9 +16,7 @@ import (
 
 func TestThriftrwListDeps(t *testing.T) {
 	tmpDir, err := ioutil.TempDir("", "")
-	if err != nil {
-		t.Skipf("error creating temporary directory: %v", err)
-	}
+	require.NoError(t, err)
 
 	defer os.RemoveAll(tmpDir)
 
@@ -33,13 +31,9 @@ include "./c.thrift"`,
 	for name, content := range exampleThrifts {
 		path := filepath.Join(tmpDir, name)
 		err = os.MkdirAll(filepath.Dir(path), 0755)
-		if err != nil {
-			t.Skipf("error mkdir %s: %v", path, err)
-		}
+		require.NoError(t, err)
 		err = ioutil.WriteFile(path, []byte(content), 0644)
-		if err != nil {
-			t.Skipf("error writing %s: %v", path, err)
-		}
+		require.NoError(t, err)
 	}
 
 	t.Run("no dependencies", func(t *testing.T) {
@@ -47,33 +41,28 @@ include "./c.thrift"`,
 		require.NoError(t, err)
 		sort.Strings(outputLines)
 		t.Logf("output lines: %+v", outputLines)
-		assert.Equal(t, 0, len(outputLines))
+		assert.Empty(t, outputLines)
 	})
 	t.Run("one dependency", func(t *testing.T) {
 		outputLines, err := listDependentThrifts(filepath.Join(tmpDir, "test/b.thrift"), tmpDir)
 		require.NoError(t, err)
 		sort.Strings(outputLines)
 		t.Logf("output lines: %+v", outputLines)
-		assert.Equal(t, 1, len(outputLines))
-		assert.Equal(t, "test/a.thrift", outputLines[0])
+		assert.Equal(t, []string{"test/a.thrift"}, outputLines)
 	})
 	t.Run("one dependency with relative path", func(t *testing.T) {
 		outputLines, err := listDependentThrifts(filepath.Join(tmpDir, "test/b.thrift"), filepath.Join(tmpDir, "test"))
 		require.NoError(t, err)
 		sort.Strings(outputLines)
 		t.Logf("output lines: %+v", outputLines)
-		assert.Equal(t, 1, len(outputLines))
-		assert.Equal(t, "a.thrift", outputLines[0])
+		assert.Equal(t, []string{"a.thrift"}, outputLines)
 	})
 	t.Run("transitive and multiple dependencies", func(t *testing.T) {
 		outputLines, err := listDependentThrifts(filepath.Join(tmpDir, "test/d.thrift"), tmpDir)
 		require.NoError(t, err)
 		sort.Strings(outputLines)
 		t.Logf("output lines: %+v", outputLines)
-		assert.Equal(t, 3, len(outputLines))
-		assert.Equal(t, "test/a.thrift", outputLines[0])
-		assert.Equal(t, "test/b.thrift", outputLines[1])
-		assert.Equal(t, "test/c.thrift", outputLines[2])
+		assert.Equal(t, []string{"test/a.thrift", "test/b.thrift", "test/c.thrift"}, outputLines)
 	})
 	t.Run("with open error", func(t *testing.T) {
 		_, err := listDependentThrifts("/does-not-exist", "")
