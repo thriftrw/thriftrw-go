2a20d5eafb45875e88c66a3f76df721f491a3ccf
Dom Narducci <dom@uber.com>
2018-10-30 16:23:41 -0700

    Reference Protocol interface from binary package documentation

--- protocol/binary/envelope.go
+++ protocol/binary/envelope.go
@@ -18,7 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-// Package binary implements the Thrift Binary protocol.
 package binary
 
 import (
