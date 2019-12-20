85870d9535913b3c88b6f8205fb49b26e28e721b
Dmitriy Shirchenko <shirchen@users.noreply.github.com>
2019-12-20 12:34:04 -0800

    Update gen/roundtrip_test.go
    Co-Authored-By: Abhinav Gupta <mail@abhinavg.net>

--- gen/roundtrip_test.go
+++ gen/roundtrip_test.go
@@ -44,7 +44,7 @@ func assertRoundTrip(t *testing.T, x thriftType, v wire.Value, msg string, args 
 		}
 		// Flip v to deserialize(serialize(x.ToWire())) to ensure full round trip.
 		freshV, ok := assertBinaryRoundTrip(t, w, message)
-		if !assert.True(t, b, "%v: failed encode/decode round trip for (%v.ToWire())) != %v", x, v) {
+		if !assert.True(t, ok, "%v: failed encode/decode round trip for (%v.ToWire())) != %v", x, v) {
 			return false
 		}
 		v = freshV // use the "freshest" value.
