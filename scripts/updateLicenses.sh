f4b1af60e8ee7e65a886a031112b2496de5507e2
r-hang <rhang@uber.com>
2023-04-03 21:53:27 -0700

    release preparation: make generate (#566)
    The thriftrw release instructions call for running `make generate` and
    seeing no changes to verify the state of dev before creating a release
    commit PR on master. This change is the result of running `make generate`.

--- scripts/updateLicenses.sh
+++ scripts/updateLicenses.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-python "$(dirname $0)"/updateLicense.py \
+python3 "$(dirname $0)"/updateLicense.py \
 	$(go list -json ./... \
 	| jq -r '.Dir + "/" + (.GoFiles | .[])' \
 	| grep -v /gen/internal/tests/ \
